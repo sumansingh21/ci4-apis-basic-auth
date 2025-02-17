baabe1fb9d1dcbf93864d8c8b807d6c6d9e23f45
sumansingh21 <sumans.bca1821@gmail.com>
2025-02-17 23:13:23 +0530

    Second Commit

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,15 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->group("product",["namespace" => "App\Controllers\Api"], function($routes){
+    $routes->post("add", "ProductController::addProduct");
+
+    $routes->get("list", "ProductController::listAllProduct");
+
+    $routes->get("/(:num)", "ProductController::getSingleProduct/$1");
+
+    $routes->put("/(:num)", "ProductController::updateProduct/$1");
+
+    $routes->delete("/(:num)", "ProductController::deleteProduct/$1");
+});
--- app/Controllers/Api/ProductController.php
+++ app/Controllers/Api/ProductController.php
@@ -11,27 +11,40 @@ class ProductController extends ResourceController
     protected $format = "json";
 
     //post - 
-    public function addProduct(){
+    public function addProduct()
+    {
+        $validationRules = [
+            "title" => [
+                "rules" => "required|min_length[3]",
+                "errors" => [
+                    "required" => "Product title is required",
+                    "min_length" => "Title must be greater than 3 character"
+                ]
+            ],
+            "cost" => [
+                "rules" => "required|integer|greater_that[0]",
+                "errors" => [
+                    "required" => "Please provide product cost",
+                    "integer" => "Cost must be an integer value",
+                    "greater_than" => "Product cost must be greater than 0 value"
+                ]
+            ],
+        ];
 
+        if(!$this->validate($validationRules)){
+            return $this->fail($this->validator->getErrors());
+        }
     }
 
     // get
-    public function listAllProducts(){
-
-    }
+    public function listAllProducts() {}
 
     // get 
-    public function getSingleProduct($product_id){
-
-    }
+    public function getSingleProduct($product_id) {}
 
     // put
-    public function updateProduct($product_id){
-
-    }
+    public function updateProduct($product_id) {}
 
     //delete
-    public function deleteProduct(){
-
-    }
+    public function deleteProduct() {}
 }
