a8a140acb5da0ddb36df974dfba4fac9385e7e4d
sumansingh21 <sumans.bca1821@gmail.com>
2025-02-18 23:01:51 +0530

    Add, fetchall, fetchSingle completed 2

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,7 +8,6 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 
 $routes->group("product",["namespace" => "App\Controllers\Api"], function($routes){
-
     $routes->post("add", "ProductController::addProduct");
 
     $routes->get("list", "ProductController::listAllProduct");
