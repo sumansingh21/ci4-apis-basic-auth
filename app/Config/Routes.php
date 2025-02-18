08046ed0a2512c1e8c52c2dc94ce42566ed7c76a
sumansingh21 <sumans.bca1821@gmail.com>
2025-02-18 23:00:32 +0530

    Add, fetchall, fetchSingle completed

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,11 +8,12 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 
 $routes->group("product",["namespace" => "App\Controllers\Api"], function($routes){
+
     $routes->post("add", "ProductController::addProduct");
 
     $routes->get("list", "ProductController::listAllProduct");
 
-    $routes->get("/(:num)", "ProductController::getSingleProduct/$1");
+    $routes->get("(:num)", "ProductController::getSingleProduct/$1");
 
     $routes->put("/(:num)", "ProductController::updateProduct/$1");
 
--- app/Controllers/Api/ProductController.php
+++ app/Controllers/Api/ProductController.php
@@ -22,7 +22,7 @@ class ProductController extends ResourceController
                 ]
             ],
             "cost" => [
-                "rules" => "required|integer|greater_that[0]",
+                "rules" => "required|integer|greater_than[0]",
                 "errors" => [
                     "required" => "Please provide product cost",
                     "integer" => "Cost must be an integer value",
@@ -34,13 +34,82 @@ class ProductController extends ResourceController
         if(!$this->validate($validationRules)){
             return $this->fail($this->validator->getErrors());
         }
+
+        $imageFile = $this->request->getFile("image");
+
+        if($imageFile){  //abc.png
+            //File is available.
+            $newProductImageName = $imageFile->getRandomName();
+
+            $imageFile->move(FCPATH ."uploads", $newProductImageName);
+
+            $productImageURL = "uploads/" . $newProductImageName;
+        } else {
+            $productImageURL = null;
+        }
+
+        $data = $this->request->getPost();
+
+        $title = $data['title'];
+        $cost = $data['cost'];
+        $description = isset($data['description']) ? $data['description'] : "";
+
+        if(  $this->model->insert([
+            "title" => $title,
+            "cost" => $cost,
+            "description" => $description,
+            "product_image" => $productImageURL,
+        ])){ 
+            return $this->respond([
+                "status" => true, 
+                "message" => "Product added successfully"
+            ]);
+        } else {
+            return $this->respond([
+                "status" => false, 
+                "message" => "Failed to add product"
+            ]);
+        }
+      
     }
 
     // get
-    public function listAllProducts() {}
+    public function listAllProduct() {
+
+        $products = $this->model->findAll();
+       
+        if($products){
+          return $this->respond([
+           "status" => true,
+           "message" => "Product Found Successfully",
+           "product" => $products
+          ]);
+        }else{
+            return $this->respond([
+                "status" => true,
+                "message" => "Not Product Found.",
+               ]);
+        }
+    }
 
     // get 
-    public function getSingleProduct($product_id) {}
+    public function getSingleProduct($product_id) {
+
+        $product = $this->model->find($product_id);
+       
+        if($product){
+          return $this->respond([
+           "status" => true,
+           "message" => "Product Found Successfully",
+           "product" => $product
+          ]);
+        }else{
+            return $this->respond([
+                "status" => true,
+                "message" => "Not Product Found.",
+               ]);
+        }
+    }
 
     // put
     public function updateProduct($product_id) {}
